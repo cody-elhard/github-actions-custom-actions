7812d6cda36cee0752270ca133f53bbbbee3cc4e
Codzter <32208000+cody-elhard@users.noreply.github.com>
2023-09-05 08:24:56 -0500

    Can we log inputs?

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -4,7 +4,18 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  // Get input values
+  const bucket = core.getInput('bucket', { required: true });
+  const region = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // Upload files to S3
+
   core.notice('Starting deploy to S3');
+  // Log all inputs
+  core.info(`Bucket: ${bucket}`);
+  core.info(`Region: ${region}`);
+  core.info(`Dist folder: ${distFolder}`);
 }
 
 run();
